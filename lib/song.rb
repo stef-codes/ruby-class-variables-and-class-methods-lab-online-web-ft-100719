6fcd2b0e0ed0fd78b8472510c621e8adff3f11be
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-18 23:33:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,10 +44,10 @@ class Song
       if artist_count[artist] 
         artist_count[artist] += 1
       else 
-        genre_count[genre] = 1
+        artist_count[artist] = 1
       end
     end
-    genre_count
+    artist_count
   end 
     
   
