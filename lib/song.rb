fa13924b9590344e07b43cfa1692b424c9a73f4b
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-18 23:30:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,9 +27,15 @@ class Song
   end 
   
   def self.genre_count 
-    genre_count = {
-      @genre => 
-    }
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre] 
+        genre_count[genre] += 1
+      else 
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end  
     
     
